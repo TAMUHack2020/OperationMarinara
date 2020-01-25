5be5d3cae1a09172313f2a8e931ba840540d5924
Liam <lbessell@tamu.edu>
2020-01-25 16:11:01 -0600

    Initial flask commit

---
+++ src/flask_sms.py
@@ -0,0 +1,18 @@
+# Twilio and Flask, 1/25/20
+
+from flask import Flask, request, redirect
+from twilio import twiml			# Twilio Markup Language
+
+app = Flask(__name__)
+
+@app.route("/sms", methods=["GET", "POST"])
+def sms_reply():
+	"""Response to incoming calls with a simple text message."""
+	
+	resp = twiml.Response()
+	resp.message("Coronavirus detected in Gate 21")
+
+	return str(resp)
+
+if __name__ == "__main__":
+	app.run(debug=True)
