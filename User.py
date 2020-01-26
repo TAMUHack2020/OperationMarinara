89744e92d0f4038f89b17f7364d18db7c66a9847
Kevin Jiang <kjiang1998@gmail.com>
2020-01-25 19:09:58 -0600

    Added user class

---
+++ User.py
@@ -0,0 +1,8 @@
+class User:
+    name = "John Smith"
+    phone = "+18295733519"
+    departureTime = "2020-01-25T02:58:32.307-06:00"
+    arrivalTime = "2020-01-25T05:12:32.307-06:00"
+    eta = "2h 11m"
+    origin = "IAH"
+    destination = "SFO"
